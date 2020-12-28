1a9f5450dfbb4bd7301e2b4e0c4f1bf7c85a1f38
Micah <micah.iserman@gmail.com>
2020-12-28 07:53:47 -0700

    adds example scripts

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Currently live at [hungry-bohr-5cc8da.netlify.app](https://hungry-bohr-5cc8da.ne
 Runs as a lambda function from [functions/api.js](./functions/api.js), and uses [dataview.js](./src/dataview.js), [data.json](./src/data.json), and [levels.json](./src/levels.json).
 
 Currently live at [hungry-bohr-5cc8da.netlify.app/.netlify/functions/api](https://hungry-bohr-5cc8da.netlify.app/.netlify/functions/api).
+
+The [examples](./examples) directory has example scripts of the API's use in R ([api_examples.R](./examples/api_examples.R)) and Python ([api_examples.py](./examples/api_examples.py)).
---
+++ examples/api_example.R
@@ -0,0 +1,49 @@
+# make URL formatting function
+arrest_explorer = function(value = 'arrests', split = '', su = '', sort = '',
+  format_table = 'tall', ...,
+  base_url = 'https://hungry-bohr-5cc8da.netlify.app/.netlify/functions/api'){
+  url = paste0(base_url, '/?value=', value, '&format_table=', format_table)
+  if(su != '') url = paste0(url, '&', su)
+  args = c(list(
+    split = paste(split, collapse = ','),
+    sort = paste(sort, collapse = ',')
+  ), as.list(substitute(...())))
+  for(p in names(args)) if(args[[p]] != ''){
+    url = paste0(url, '&', p, '=', args[[p]])
+  }
+  message('reading in table from:\n', url)
+  read.csv(url)
+}
+
+# load plotting package
+if(!require(splot)){
+  install.packages('splot')
+  library(splot)
+}
+
+# examples
+
+## total arrests
+arrests = arrest_explorer()
+splot(arrests ~ Year, arrests, line = 'connected', mar = c(2, 4, 0, 0))
+
+## How did arrest rates vary by sex?
+arrests_by_sex = arrest_explorer(split = 'gender')
+splot(arrests ~ gender, arrests_by_sex, type = 'bar', mar = c(2, 4, 0, 0))
+
+## Over time, which age groups were most likely to be re-arrested?
+rearrest_by_agegroup = arrest_explorer('arrests_per_arrestee', 'age_group')
+splot(
+  arrests_per_arrestee ~ Year * age_group, rearrest_by_agegroup,
+  line = 'connected', myl = c(1.05, 1.35), leg = 'outside'
+)
+
+## What were people most frequently arrested for?
+charge_by_category = arrest_explorer(
+  'arrest_charges', 'offense_category',
+  'offense_category[mean]>10000', 'offense_category[mean]'
+)
+splot(
+  arrest_charges ~ offense_category, charge_by_category,
+  sort = TRUE, type = 'bar', mar = c(2, 4, 0, 0)
+)
