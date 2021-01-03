3f2aa023e4957e2381d810423f7e0c3da95c9436
Micah <micah.iserman@gmail.com>
2021-01-03 02:27:26 -0700

    changes outputDir

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.VUE_APP_PATH,
-  outputDir: process.env.VUE_APP_PATH,
+  outputDir: "dist" + process.env.VUE_APP_PATH,
   transpileDependencies: ["vuetify"],
 };
