f08ae281f69c062bce3ba7d788fc4350569eb702
Karl Gruschow <48029452+kgruschow@users.noreply.github.com>
2021-09-10 12:44:23 -0500

    New R API Example

--- examples/api_example.R
+++ examples/api_example.R
@@ -1,50 +1,98 @@
-# make URL formatting function
-arrest_explorer = function(value = 'arrests', split = '', su = '', sort = '',
-  format_table = 'tall', ...,
-  base_url = 'https://icjia.illinois.gov/arrestexplorer/api'){
-  url = paste0(base_url, '/?value=', value, '&format_table=', format_table)
+# ICJIA CHRI Arrest Explorer API example in R
+
+library(data.table) # this merely provides the fread package, tidyverse or the base csv read would also work
+library(ggplot2) # the most widely plot, merely used to demonstrate the simplicity of plotting the data in R
+
+# this example defines a custom function in R that eases use of the API
+
+CHRI_api <- function(value = 'arrests',
+
+                     # value options are
+                     # arrests
+                     # arrestees
+                     # arrests_per_arrestee
+                     # arrest_charges
+
+                     split = '',
+
+                     # one or two arguments as string seperated by
+                     # variables
+                     # county, crime_type, offense_category
+                     # offense_class, race, age_group, gender
+
+                     #arguments can be filtered using "attributes"/"aspects"
+
+                     # label, max, min, sum, mean
+
+                     # e.g. instead of offense_class, offense_class[mean]>1000
+
+                     su = '',
+
+
+                     # filter arguments, using same variables as split argument
+                     # arguments can be filtered using
+                     # label, max, min, sum, mean
+
+                     # e.g. instead of offense_class, offense_class[mean]>1000
+
+                     sort = '',
+
+                     # +/- variable[attribute] and ordered
+                     # e.g. county, -race[mean]
+
+
+                     format_table = 'tall',
+
+                     # tall, mixed, wide
+
+                     ..., # advanced function see substitute call below
+
+                      base_url = 'https://icjia.illinois.gov/arrestexplorer/api')
+
+
+{
+  #put a valid URL together from the above function call
+  url <-  paste0(base_url, '/?value=', value, '&format_table=', format_table)
+ # if there any filter su arguments, insert them into the above.
   if(su != '') url = paste0(url, '&', su)
+
+ # process additional ... arguments into the call
   args = c(list(
     split = paste(split, collapse = ','),
     sort = paste(sort, collapse = ',')
   ), as.list(substitute(...())))
-  for(p in names(args)) if(args[[p]] != ''){
+
+  for (p in names(args)) if (args[[p]] != '')
+    {
     url = paste0(url, '&', p, '=', args[[p]])
   }
+
+  # provider user feedback showing the URL
   message('reading in table from:\n', url)
-  res = GET(url)
-  if(res$status_code != 200){
-    stop('request failed:\n  ', content(res, 'text', encoding = 'utf-8'), call. = FALSE)
-  }else read.csv(text = content(res, 'text', encoding = 'utf-8'))
+
+  # read the url in, the API returns a CSV, fread is a very fast csv read function that outputs
+  # a data.table type of dataframe.
+  fread(url)
 }
 
-# load http and plotting packages
-library(httr)
-library(splot)
+arrests <- CHRI_api() # without arguments the CHRI_api will return annual total arrests
+# an example of how to plot this data using the common ggplot function
+ggplot(data = arrests, mapping = aes(x=Year, y = arrests)) + geom_line()
 
-# examples
+# demonstration of a call for arrestees by gender
+working_call <- CHRI_api(value = 'arrestees',split='gender')
 
-## total arrests
-arrests = arrest_explorer()
-splot(arrests ~ Year, arrests, line = 'connected', mar = c(2, 4, 0, 0))
+# this is a counterexample, and will fail because the su argument is invalid
+arresstees_by_gender_race_incorrectly <- CHRI_api(value = 'arrestees',split='gender', su = 'race=White')
 
-## How did arrest rates vary by sex?
-arrests_by_sex = arrest_explorer(split = 'gender')
-splot(arrests ~ gender, arrests_by_sex, type = 'bar', mar = c(2, 4, 0, 0))
+# this API was built to be quite conservative about accepting incorrect input
 
-## Over time, which age groups were most likely to be re-arrested?
-rearrest_by_agegroup = arrest_explorer('arrests_per_arrestee', 'age_group')
-splot(
-  arrests_per_arrestee ~ Year * age_group, rearrest_by_agegroup,
-  line = 'connected', myl = c(1.05, 1.35), leg = 'outside'
-)
+# the final example is based on the below string, which was copied from the UI using the export menu
+# https://icjia.illinois.gov/arrestexplorer/api/?format_table=tall&value=arrestees&split=race,county&county[label]=douglas,dupage,edgar,edwards
 
-## What were people most frequently arrested for?
-charge_by_category = arrest_explorer(
-  'arrest_charges', 'offense_category',
-  'offense_category[mean]>10000', 'offense_category[mean]'
-)
-splot(
-  arrest_charges ~ offense_category, charge_by_category,
-  sort = TRUE, type = 'bar', mar = c(2, 4, 0, 0)
-)
+
+# this replicates the above call in the API
+test <- CHRI_api(value = 'arrestees', split ='race,county', su = 'county[label]=champaign,dupage,edgar,edwards')
+
+# Note that there are also options in the API such as &format_category=indices which returns numeric codes instead of text labels, and can be inserted into the
+# the URL.
--- examples/api_example.py
+++ examples/api_example.py
@@ -1,70 +1,76 @@
 # make URL formatting function
-def arrest_explorer(value = 'arrests', split = '', su = '',
-  sort = '', format_table = 'tall',
-  base_url = 'https://icjia.illinois.gov/arrestexplorer/api',
-  **args):
-  url = base_url + '/?value=' + value + '&format_table=' + format_table
-  if su != '':
-    url += '&' + su
-  if split != '':
-    if not isinstance(split, str):
-      split = ','.join(split)
-    args['split'] = split
-  if sort != '':
-    if not isinstance(sort, str):
-      sort = ','.join(sort)
-    args['sort'] = sort
-  for k in args:
-    url += '&' + k + '=' + args[k]
-  print('reading in table from:\n', url)
-  res = requests.get(url)
-  if res.status_code != 200:
-    print('request failed:\n', res.text)
-  else:
-    return pandas.read_csv(io.StringIO(res.text))
+import matplotlib.pyplot
+import pandas
+import io
+import requests
+
+
+def arrest_explorer(value='arrests', split='', su='',
+                    sort='', format_table='tall',
+                    base_url='https://icjia.illinois.gov/arrestexplorer/api',
+                    **args):
+    url = base_url + '/?value=' + value + '&format_table=' + format_table
+    if su != '':
+        url += '&' + su
+    if split != '':
+        if not isinstance(split, str):
+            split = ','.join(split)
+        args['split'] = split
+    if sort != '':
+        if not isinstance(sort, str):
+            sort = ','.join(sort)
+        args['sort'] = sort
+    for k in args:
+        url += '&' + k + '=' + args[k]
+    print('reading in table from:\n', url)
+    res = requests.get(url)
+    if res.status_code != 200:
+        print('request failed:\n', res.text)
+    else:
+        return pandas.read_csv(io.StringIO(res.text))
+
 
 # load http, data, and plotting packages
-import requests, io, pandas, matplotlib.pyplot
 
 # examples
 
-## total arrests
+# total arrests
 arrests = arrest_explorer()
 fig, ax = matplotlib.pyplot.subplots()
-ax.plot('Year', 'arrests', data = arrests)
+ax.plot('Year', 'arrests', data=arrests)
 ax.set_title('Arrests by Year')
 ax.set_ylabel('Arrests')
 ax.set_xlabel('Year')
 
-## How did arrest rates vary by sex?
-arrests_by_sex = arrest_explorer(split = 'gender')
+# How did arrest rates vary by sex?
+arrests_by_sex = arrest_explorer(split='gender')
 fig, ax = matplotlib.pyplot.subplots()
-ax.bar('gender', 'arrests', data = arrests_by_sex)
+ax.bar('gender', 'arrests', data=arrests_by_sex)
 ax.set_title('Arrests by Gender')
 ax.set_ylabel('Arrests')
 ax.set_xlabel('Gender')
 
-## Over time, which age groups were most likely to be re-arrested?
+# Over time, which age groups were most likely to be re-arrested?
 rearrest_by_agegroup = arrest_explorer('arrests_per_arrestee', 'age_group')
 fig, ax = matplotlib.pyplot.subplots()
 for group in rearrest_by_agegroup.age_group.unique():
-  ax.plot(
-    'Year', 'arrests_per_arrestee', label = group,
-    data = rearrest_by_agegroup[rearrest_by_agegroup.age_group == group]
-  )
+    ax.plot(
+        'Year', 'arrests_per_arrestee', label=group,
+        data=rearrest_by_agegroup[rearrest_by_agegroup.age_group == group]
+    )
 ax.set_title('Arrests Per Arrestee by Year between Age Group')
 ax.set_ylabel('Arrests Per Arrestee')
 ax.set_xlabel('Year')
 ax.legend()
 
-## What were people most frequently arrested for?
+# What were people most frequently arrested for?
 charge_by_category = arrest_explorer(
-  'arrest_charges', 'offense_category',
-  'offense_category[mean]>10000', 'offense_category[mean]'
+    'arrest_charges', 'offense_category',
+    'offense_category[mean]>10000', 'offense_category[mean]'
 )
 fig, ax = matplotlib.pyplot.subplots()
-ax.bar('offense_category', 'arrest_charges', data = charge_by_category)
-matplotlib.pyplot.setp(ax.get_xticklabels(), rotation = 40, ha = 'right')
+ax.bar('offense_category', 'arrest_charges', data=charge_by_category)
+matplotlib.pyplot.setp(ax.get_xticklabels(), rotation=40, ha='right')
 ax.set_title('Arrest Charges by Offense Category')
 ax.set_ylabel('Arrest Charges')
 ax.set_xlabel('Offense Category')
